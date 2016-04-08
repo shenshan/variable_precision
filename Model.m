7b7ad35e65f6de6affa3beec7eea68a294249e50
Shan Shen <shanshen@shans-mac-pro.ad.bcm.edu>
2016-04-08 11:44:43 -0500

    Add a field npars into the Table Model

--- Model.m
+++ Model.m
@@ -1,9 +1,10 @@
 %{
 varprecision.Model (manual) # models considered
 -> varprecision.Experiment
-model_name   : varchar(50)        # model names 
------
-notes=null         : varchar(256)       # notes for this model
+model_name      : varchar(50)            # model names
+---
+notes=null                  : varchar(256)                  # notes for this model
+npars                       : tinyint                       # number of parameters
 %}
 
 classdef Model < dj.Relvar
