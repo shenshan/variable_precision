c6134f931d082dd167e5d245cc75f2c21d840b6b
Shan Shen <shanshen@shans-mac-pro.ad.bcm.edu>
2016-04-25 10:29:24 -0500

    First implementation of plotting the params regression for fake data test.

---
+++ +plots/showParsRegression.m
@@ -0,0 +1,99 @@
+function showParsRegression(varargin)
+%SHOWPARSREGRESSION shows true parameters versus the fit parameters 
+%   function showParsRegression(varargin)
+%   varargin includes restriction of experiments and model
+
+subjs = fetch(varprecision.Subject & 'subj_type="fake"');
+exps = fetch(varprecision.Experiment & varargin);
+
+wid = 30;
+height = 40;
+edge = 10;
+
+for iexp = exps'
+    
+    models = fetch(varprecision.Model & iexp & varargin);
+    setsizes = fetch1(varprecision.Experiment & iexp, 'setsize');
+    nlambda = length(setsizes);
+    
+    for imodel = models'
+        % fetch corresponding tables for real parameters and fit parameters
+        subj = fetch(varprecision.Subject & subjs & ['model_name="' imodel.model_name '"']);
+        [p_rightMat,lambdaMat,thetaMat,guessMat] = fetchn(varprecision.FakeDataParams & imodel & subj, 'p_right','lambda','theta','guess');
+        [p_rightMat_fit,lambdaMat_fit,thetaMat_fit,guessMat_fit] = fetchn(varprecision.FitParametersEvidence & imodel & subj, 'p_right_hat','lambda_hat','theta_hat','guess_hat');
+        
+        p_right_real = varprecision.utils.decell(p_rightMat);
+        lambda_real = squeeze(varprecision.utils.decell(lambdaMat));
+        
+        p_right_fit = varprecision.utils.decell(p_rightMat_fit);
+        lambda_fit = squeeze(varprecision.utils.decell(lambdaMat_fit));
+        
+        switch imodel.model_name
+            case 'CP'
+                fig = Figure(101,'size',[wid*(nlambda+1)+edge,height]); hold on
+                subplot(1,nlambda+1,1)
+                scatter(p_right_real,p_right_fit,'o')
+                refline(1)
+                for ii = 1:nlambda
+                    subplot(1,nlambda+1,ii+1)
+                    scatter(lambda_real(ii,:),lambda_fit(ii,:),'o')
+                    refline(1)
+                end
+            case 'CPG'
+                guess_real = varprecision.utils.decell(guessMat);
+                guess_fit = varprecision.utils.decell(guessMat_fit);
+                fig = Figure(102,'size',[wid*(nlambda+2)+edge,height]); hold on
+                subplot(1,nlambda+2,1)
+                scatter(p_right_real,p_right_fit,'o')
+                refline(1)
+                for ii = 1:nlambda
+                    subplot(1,nlambda+2,ii+1)
+                    scatter(lambda_real(ii,:),lambda_fit(ii,:),'o')
+                    refline(1)
+                end
+                subplot(1,nlambda+2,nlambda+2)
+                scatter(guess_real,guess_fit,'o')
+                refline(1)
+            case 'VP'
+                theta_real = varprecision.utils.decell(thetaMat);
+                theta_fit = varprecision.utils.decell(thetaMat_fit);
+                fig = Figure(103,'size',[wid*(nlambda+2)+edge,height]); hold on
+                subplot(1,nlambda+2,1)
+                scatter(p_right_real,p_right_fit,'o')
+                refline(1)
+                for ii = 1:nlambda
+                    subplot(1,nlambda+2,ii+1)
+                    scatter(lambda_real(ii,:),lambda_fit(ii,:),'o')
+                    refline(1)
+                end
+                subplot(1,nlambda+2,nlambda+2)
+                scatter(theta_real,theta_fit,'o')
+                refline(1)
+            case 'VPG'
+                theta_real = varprecision.utils.decell(thetaMat);
+                theta_fit = varprecision.utils.decell(thetaMat_fit);
+                guess_real = varprecision.utils.decell(guessMat);
+                guess_fit = varprecision.utils.decell(guessMat_fit);
+                fig = Figure(104,'size',[wid*(nlambda+3)+edge,height]); hold on
+                subplot(1,nlambda+3,1)
+                scatter(p_right_real,p_right_fit,'o')
+                refline(1)
+                for ii = 1:nlambda
+                    subplot(1,nlambda+3,ii+1)
+                    scatter(lambda_real(ii,:),lambda_fit(ii,:),'o')
+                    refline(1)
+                end
+                subplot(1,nlambda+3,nlambda+2)
+                scatter(theta_real,theta_fit,'o')
+                refline(1)
+                subplot(1,nlambda+3,nlambda+3)
+                scatter(guess_real,guess_fit,'o')
+                refline(1)
+        end
+        fig.cleanup
+    end
+    
+end
+
+
+
