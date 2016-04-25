8f2f67e8f4c925da081355faf57177bae4b8a810
Shan Shen <shanshen@shans-mac-pro.ad.bcm.edu>
2016-04-25 15:55:10 -0500

    Corresponding changes in the inserting function in order to match the new table defination of the table Subject.

--- scripts/insertFakeSubjs.m
+++ scripts/insertFakeSubjs.m
@@ -1,14 +1,15 @@
 % insert fake subjects into the subject table
 
 model_names = {'CP','CPG','VP','VPG'};
-fake_ids = 1:20;
+fake_ids = 21:40;
 
 for ii = 1:length(model_names)
-    key.model_name = model_names{ii};
+    key.model_gene = model_names{ii};
     for jj = 1:length(fake_ids)
         key.subj_type = 'fake';
         key.fake_idx = fake_ids(jj);
         key.subj_sex = 'Unknown';
+        key.fake_param_method = 'random'; 
         key.subj_initial = [key.model_name '_' num2str(key.fake_idx)];
         insert(varprecision.Subject, key);
     end
