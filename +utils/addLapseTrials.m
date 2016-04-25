56c8b16748f5296b984aa650eef2c95a68c608ba
Shan Shen <shanshen@shans-mac-pro.ad.bcm.edu>
2016-04-25 13:16:43 -0500

    Change the method to add lapse rate.

--- +utils/addLapseTrials.m
+++ +utils/addLapseTrials.m
@@ -4,9 +4,5 @@ function response = addLapseTrials(response,guess)
 % SS 04-21-16
 %   response is a vector with values 1 or -1, guess is a number from 0 to 1
 
-nTrials = length(response);
-
-nLapseTrials = binornd(nTrials, guess);
-
-response(1:nLapseTrials) = randi(2,size(1:nLapseTrials))*2-3;
-
+idx = rand(size(response))<guess;
+response = idx .* (2*randi(2,size(response))-3) + (1-idx) .* response;
