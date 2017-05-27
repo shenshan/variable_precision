0d4af01c624bcf21057afcf8d25143daec48246a
Shan Shen <shanshen@shans-mac-pro.ad.bcm.edu>
2017-05-26 21:47:16 -0500

    Add tables to compute fit predictions based on the best params of average llmax etc.

---
+++ FitPredictionBpsBest2DAvg.m
@@ -0,0 +1,34 @@
+%{
+varprecision.FitPredictionBpsBest2DAvg (computed) # compute prediction to plot in 2d.
+->varprecision.FitPredictionBpsBestAvg
+-----
+prediction_plot_2d: longblob   # 2 dimensional matrix, the first dimension is target orientation, the second dimension is distractor orientation
+%}
+
+classdef FitPredictionBpsBest2DAvg < dj.Relvar & dj.AutoPopulate
+    
+    properties
+        popRel = varprecision.FitPredictionBpsBestAvg & varprecision.DataStats
+    end
+	methods(Access=protected)
+
+		function makeTuples(self, key)
+            
+            [stims,dist,idx1,idx2] = fetch1(varprecision.DataStats2D & key, 'stims','dist','idx_target','idx_dist');
+            prediction = fetch1(varprecision.FitPredictionBpsBest & key, 'prediction');
+            
+            prediction_plot_2d = zeros(length(stims),length(dist));
+            
+            for ii = 1:length(stims)
+                for jj = 1:length(dist)
+                    prediction_plot_2d(ii,jj) = mean(prediction(idx1==ii & idx2==jj));
+                end
+            end
+
+            key.prediction_plot_2d = prediction_plot_2d;
+            
+			self.insert(key)
+		end
+	end
+
+end
